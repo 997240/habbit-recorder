69eb085252035e3eb988c7fe3b895d883f0ad057
5组-杨普元 <yangpuyuan123@gmail.com>
2025-09-11 09:50:40 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// @ts-ignore
+const PUBLIC_URL = process?.env?.PUBLIC_URL;
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/habbit-recorder/', 
+  base: PUBLIC_URL || '/habbit-recorder/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
