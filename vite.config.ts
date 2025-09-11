bbf7cf971e18914fa11a833d5ab27049b0adaf02
5组-杨普元 <yangpuyuan123@gmail.com>
2025-09-11 09:35:27 +0800

    reamde file updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/habbit-recorder/', 
+  // base: '/habbit-recorder/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
